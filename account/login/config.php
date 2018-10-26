e5d4fdbcc15561f70e469ac927e1b0534daed839
Richard Bao <richardbao419@gmail.com>
2018-10-25 17:34:28 -0700

    update

--- account/login/config.php
+++ account/login/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'adminuNAcNgm');
-define('DB_PASSWORD', 'JfkT5nAHRqu3');
-define('DB_DATABASE', 'webdev');
+define('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net');
+define('DB_USER', 'ba4712a6c49735');
+define('DB_PASSWORD', 'af25d1e1');
+define('DB_DATABASE', 'heroku_28dbc20876ed2b4');
