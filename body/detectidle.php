99ae525aa67ad7c5b3b57ec925c534105b2b87a1
Richard Bao <richardbao419@gmail.com>
2018-10-25 21:23:47 -0700

    fix

--- body/detectidle.php
+++ body/detectidle.php
@@ -1,6 +1,6 @@
 <?php
 require('../account/login/config.php');
-    $link = mysqli_connect($link, DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+    $link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
     $username = $_SESSION['SESS_USER_NAME'];
     //$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
     $last = time();
